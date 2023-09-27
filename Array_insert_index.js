5142ad08523e01ab421297551ddca22df5ad29ac
chaitanya <[parandkarchaitanya@gmail.com]>
2023-09-27 08:42:23 +0530

    Adding array insert code to repo

---
+++ Array_insert_index.js
@@ -0,0 +1,24 @@
+function insertIndex(arr, sizeofArray, index, element)
+{
+    /*var temp;
+    if(index<sizeofArray)
+    {
+        temp=arr[index];
+        arr[index]=element;
+        for (i=index+1;i<sizeofArray;i++)
+        {
+            element=arr[i];
+            arr[i]=temp;
+            temp=element;
+        }
+    }
+    else
+    {
+        arr[sizeofArray-1]= -1;
+    }
+    console.log(arr)*/
+    arr.splice(3,0,3)
+    console.log(arr)
+}
+/* Both the codes are working properly but somehow the above one has given given time complexity error though it is the same one*/
+insertIndex([7,7,1,1], 5, 3, 3)
