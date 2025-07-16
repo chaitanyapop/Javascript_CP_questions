63316e53242a8debaea949f5a9e0601b9e4fca8a
chaitanyapop <parandkarchaitanya@gmail.com>
2025-07-16 09:34:35 +0530

    Remove duplicates from array

---
+++ Arrays/Remove_duplicates.js
@@ -0,0 +1,15 @@
+"use strict";
+// condition duplicate elements are in sorted order
+function removeDuplicates(arr) {
+    arr = arr.sort();
+    let properArrayMark = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] != arr[properArrayMark]) {
+            arr[properArrayMark + 1] = arr[i];
+            properArrayMark = properArrayMark + 1;
+        }
+    }
+    arr.length = properArrayMark + 1;
+    return arr;
+}
+console.log(removeDuplicates([1, 4, 3, 2, 1, 3]));
