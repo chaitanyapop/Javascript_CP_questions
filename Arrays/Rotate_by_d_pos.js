0bfb67b24463e69fdd6f64ce922e0c274fe634b1
chaitanyapop <parandkarchaitanya@gmail.com>
2025-07-21 09:25:17 +0530

    Rotate array by n position

---
+++ Arrays/Rotate_by_d_pos.js
@@ -0,0 +1,21 @@
+"use strict";
+function rotateByDpos(arr, d) {
+    d = d % arr.length;
+    arr = arrayRotation(arr, 0, d - 1); // first rotate d-1 element
+    arr = arrayRotation(arr, d, arr.length - 1); // then rotate d to length-1 elements
+    arr = arrayRotation(arr, 0, arr.length - 1); // rotate whole array
+    return arr;
+}
+function arrayRotation(arr, start, end) {
+    let temp = 0;
+    while (start <= end) {
+        temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+    return arr;
+}
+console.log(rotateByDpos([2, 4, 6, 8, 10], 32));
+// This is array of reversal algorithm
