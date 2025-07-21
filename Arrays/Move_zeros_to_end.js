3484fa04458d2c56084beb10584e36cbfc9e0da6
chaitanyapop <parandkarchaitanya@gmail.com>
2025-07-21 08:37:43 +0530

    move zeros to end

--- Arrays/Move_zeros_to_end.js
+++ Arrays/Move_zeros_to_end.js
@@ -1,21 +1,16 @@
 "use strict";
 function moveZerosToEnd(arr) {
-  let positionOfZero;
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] != 0 && positionOfZero != undefined) {
-      arr[positionOfZero] = arr[i];
-      arr[i] = 0;
-      if (arr[positionOfZero + 1] == 0) {
-        positionOfZero = positionOfZero + 1;
-      } else {
-        positionOfZero = i;
-      }
-    } else if (arr[i] == 0 && positionOfZero == undefined) {
-      positionOfZero = i;
+    let positionOfZero;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] != 0 && positionOfZero != undefined) {
+            arr[positionOfZero] = arr[i];
+            arr[i] = 0;
+            positionOfZero = i;
+        }
+        else if (arr[i] == 0 && positionOfZero == undefined) {
+            positionOfZero = i;
+        }
     }
-  }
-  return arr;
+    return arr;
 }
-console.log(
-  moveZerosToEnd([3, 3, 4, 0, 0, 0, 1, 2, 3, 0, 4, 0, 0, 5, 6, 0, 6])
-);
+console.log(moveZerosToEnd([0, 0, 0, 10, 0]));
