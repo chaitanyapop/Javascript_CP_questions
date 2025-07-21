fdd2aa017a626431420f7e81a7498c5309f65a0c
chaitanyapop <parandkarchaitanya@gmail.com>
2025-07-21 15:32:46 +0530

    Leaders in array problem

---
+++ Arrays/Leader_in_array.js
@@ -0,0 +1,15 @@
+"use strict";
+function leaderInArray(a) {
+    let currEle = a[a.length - 1];
+    let position = a.length - 1;
+    for (let i = a.length - 2; i >= 0; i--) {
+        if (a[i] >= currEle) {
+            position = position - 1;
+            currEle = a[i];
+            a[position] = currEle;
+        }
+    }
+    a = a.slice(position, a.length);
+    return a;
+}
+console.log(leaderInArray([61, 61, 17]));
