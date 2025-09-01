ecb4bcf789f0d61564c7aceef6e8477c9323529f
chaitanyapop <parandkarchaitanya@gmail.com>
2025-09-01 08:41:42 +0530

    Maximum difference

---
+++ Arrays/Maximum_difference.ts
@@ -0,0 +1,19 @@
+function Maximum_difference(arr: number[]) {
+  let maxDifference = 0;
+  let j = arr.length - 1;
+  let i = arr.length - 2;
+
+  while (i >= 0) {
+    if (arr[j] > arr[i]) {
+      maxDifference = Math.max(maxDifference, arr[j] - arr[i]);
+      i--;
+    } else {
+      j = i;
+      i = i - 1;
+    }
+  }
+
+  return maxDifference;
+}
+
+console.log(Maximum_difference([20, 28, 30, 5, 25]));
