98f534d18c12fd1092a8b7a6ace5f13d7e2ce4e7
chaitanyapop <parandkarchaitanya@gmail.com>
2025-10-01 09:38:36 +0530

    Longest substring

---
+++ Arrays/Sliding_window/Longest_substring.ts
@@ -0,0 +1,24 @@
+function longestSubString(str: string): number {
+  let lastSeen: { [key: string]: number } = {};
+  let left = 0;
+  let finalMax = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    const ch = str[i];
+
+    // If character is inside current window, move left pointer
+    if (lastSeen[ch] !== undefined && lastSeen[ch] >= left) {
+      left = lastSeen[ch] + 1;
+    }
+
+    // Update last seen position
+    lastSeen[ch] = i;
+
+    // Update max length
+    finalMax = Math.max(finalMax, i - left + 1); // here +1 indicates include current character now
+  }
+
+  return finalMax;
+}
+
+console.log(longestSubString("abcdabcbeghijk"));
