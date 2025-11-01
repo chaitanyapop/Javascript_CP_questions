5bd945e01923cb519a88c3fd6a24cd18d9867d2c
chaitanyapop <parandkarchaitanya@gmail.com>
2025-11-01 13:59:37 +0530

    Majority ele

---
+++ Objects/Majority_ele.js
@@ -0,0 +1,20 @@
+"use strict";
+function majorityElement(arr) {
+    let obj = {};
+    let maxAppearedElement = arr[0];
+    let maxCal = Math.round(arr.length / 2);
+    for (let i = 0; i < arr.length; i++) {
+        if (obj[arr[i]] == undefined) {
+            obj[arr[i]] = 1;
+        }
+        else {
+            obj[arr[i]] = obj[arr[i]] + 1;
+            if (maxAppearedElement != arr[i] &&
+                obj[maxAppearedElement] < obj[arr[i]]) {
+                maxAppearedElement = arr[i];
+            }
+        }
+    }
+    return obj[maxAppearedElement] >= maxCal ? maxAppearedElement : 0;
+}
+console.log(majorityElement([3, 2, 3]));
