cfe2ce52f2f9f5c494f2a397479045a9bc0ac389
chaitanyapop <parandkarchaitanya@gmail.com>
2025-12-09 11:23:24 +0530

    understandding concept of Array and Sort function in array

--- Objects/Decreasing_freq.js
+++ Objects/Decreasing_freq.js
@@ -16,3 +16,27 @@ function decreasingFreq(arr) {
     return result;
 }
 console.log(decreasingFreq([5, 5, 4, 6, 4]));
+/*concepts=
+ result.push(...Array(freq).fill(value));
+ 
+Array(3)  means ['','',''] create array of mentioned number
+.fill(5) means fill the array with 5 so it will be [5,5,5]
+result.push(...Array(freq).fill(value)) Array(freq).fill(value) will form a value and using spread operator we are spreading that array and pushing into the result
+
+Sort concept ->
+scenario 1 (ascending) -
+[3,10] a=3 b=10
+.sort((a,b) => a-b) always consider a,b from circular brackets which will reduce confusion
+a-b < 0 a should come first and then b
+a-b > 0 b should come first and then a
+a-b = 3-10 = -7 i.e a should come first and then b
+therefore, final output becomes [3,10]
+
+scenario 2 (descending) -
+[3,10] a=10, b=3
+.sort((a,b) => b-a) always consider a,b from circular brackets which will reduce confusion
+b-a < 0 a should come first and then b
+b-a > 0 b should come first and then a
+b-a = 3-10 = -7 i.e a should come first and then b
+therefore, final output becomes [10,3]
+*/
