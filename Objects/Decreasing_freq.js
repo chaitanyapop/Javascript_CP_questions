ae2b7109d7632bcf6c1b7251114811eb769545bd
chaitanyapop <parandkarchaitanya@gmail.com>
2025-12-09 10:08:33 +0530

    decreasing order

---
+++ Objects/Decreasing_freq.js
@@ -0,0 +1,18 @@
+"use strict";
+function decreasingFreq(arr) {
+    let mapArray = new Map();
+    for (let i = 0; i < arr.length; i++) {
+        mapArray.set(arr[i], (mapArray.get(arr[i]) || 0) + 1);
+    }
+    arr = Array.from(mapArray.entries()).sort((a, b) => {
+        if (b[1] == a[1])
+            return a[0] - b[0]; //if same frequency sort in ascending
+        return b[1] - a[1];
+    });
+    let result = [];
+    arr.forEach(([value, freq]) => {
+        result.push(...Array(freq).fill(value));
+    });
+    return result;
+}
+console.log(decreasingFreq([5, 5, 4, 6, 4]));
--- Objects/Non_repeat.js
+++ Objects/Non_repeat.js
@@ -1,19 +1,20 @@
 "use strict";
 function nonRepeat(arr) {
-  let nonRepeatCount = [];
-  let objectOfFrequency = {};
-  for (let i = 0; i < arr.length; i++) {
-    if (objectOfFrequency[arr[i]] == undefined) {
-      objectOfFrequency[arr[i]] = 1;
-    } else {
-      objectOfFrequency[arr[i]] = objectOfFrequency[arr[i]] + 1;
+    let nonRepeatCount = [];
+    let objectOfFrequency = {};
+    for (let i = 0; i < arr.length; i++) {
+        if (objectOfFrequency[arr[i]] == undefined) {
+            objectOfFrequency[arr[i]] = 1;
+        }
+        else {
+            objectOfFrequency[arr[i]] = objectOfFrequency[arr[i]] + 1;
+        }
     }
-  }
-  Object.keys(objectOfFrequency).forEach((val) => {
-    if (objectOfFrequency[val] == 1) {
-      nonRepeatCount.push(Number(val));
-    }
-  });
-  return nonRepeatCount;
+    Object.keys(objectOfFrequency).forEach((val) => {
+        if (objectOfFrequency[val] == 1) {
+            nonRepeatCount.push(Number(val));
+        }
+    });
+    return nonRepeatCount;
 }
-console.log(nonRepeat([1, 1, 1, 1, 1]));
+console.log(nonRepeat([1, 1, 2, 2, 3, 3, 4, 5, 6, 7]));
