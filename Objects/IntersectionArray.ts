2e1698365a1c0ee3306d4a04f638a4e7b929ebf8
chaitanyapop <parandkarchaitanya@gmail.com>
2025-12-16 09:14:21 +0530

    array intersection

---
+++ Objects/IntersectionArray.ts
@@ -0,0 +1,14 @@
+function interSectionArray(a: number[], b: number[]) {
+  let map = new Map();
+  let interSection = 0;
+
+  for (let i = 0; i < a.length; i++) {
+    map.set(a[i], 1);
+  }
+  for (let i = 0; i < b.length; i++) {
+    map.has(b[i]) ? interSection++ : "";
+  }
+  return interSection;
+}
+
+console.log(interSectionArray([1, 2, 3, 4, 5, 6], [3, 4, 5, 6, 7]));
