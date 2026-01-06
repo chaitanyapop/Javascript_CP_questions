231c0abc950b1c6a85f01088cdb545213ff27ffb
chaitanyapop <parandkarchaitanya@gmail.com>
2026-01-06 13:06:58 +0530

    zero sum and duplicates

---
+++ Objects/Union_duplicates.ts
@@ -0,0 +1,12 @@
+function unionDuplicates(a: number[], b: number[]) {
+  let elementsInArray = new Map();
+  for (let i = 0; i < a.length; i++) {
+    elementsInArray.has(a[i]) ? "" : elementsInArray.set(a[i], true);
+  }
+  for (let i = 0; i < b.length; i++) {
+    elementsInArray.has(b[i]) ? "" : elementsInArray.set(b[i], true);
+  }
+  return [...elementsInArray.keys()];
+}
+
+console.log(unionDuplicates([1, 2, 3, 4], [2, 2, 3, 2, 2]));
---
+++ Objects/ZeroSum.ts
@@ -0,0 +1,17 @@
+function zeroSumSubArray(arr: number[]) {
+  let prefixMap = new Map();
+  let count = 0;
+  let prefixSum = 0;
+  prefixMap.set(0, 1);
+  for (let i = 0; i < arr.length; i++) {
+    prefixSum = prefixSum + arr[i];
+    if (prefixMap.has(prefixSum)) {
+      count = count + prefixMap.get(prefixSum);
+      prefixMap.set(prefixSum, prefixMap.get(prefixSum) + 1);
+    } else {
+      prefixMap.set(prefixSum, 1);
+    }
+  }
+  return count;
+}
+console.log(zeroSumSubArray([0, 0, 5, 5, 0, 0]));
